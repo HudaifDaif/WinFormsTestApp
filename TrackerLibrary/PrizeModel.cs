2743f416e638e6e524e6de3bfeedc9713d434297
Hudaifa Temsamani <107198955+HudaifDaif@users.noreply.github.com>
2024-04-10 18:15:56 +0100

    update PrizeModel with constructor that will take strings and parse to carrect type or assign default value

--- TrackerLibrary/PrizeModel.cs
+++ TrackerLibrary/PrizeModel.cs
@@ -16,10 +16,44 @@ public class PrizeModel
     /// <summary>
     /// Represents the string identifier for the place.
     /// </summary>
-    public required string PlaceName { get; set; }
+    public string PlaceName { get; set; }
 
     /// <summary>
     /// Represents the decimal value of the prize.
     /// </summary>
     public decimal PrizeAmount { get; set; }
+
+    /// <summary>
+    /// Represents a percantage value of the prize from the prize fund. This is a fraction of 1 (0.25 is 25%, 0.69 is 69%, etc.).
+    /// </summary>
+    public double PrizePercentage { get; set; }
+
+    public PrizeModel()
+    {
+
+    }
+
+    /// <summary>
+    /// String parameter constructor for use with creating object from user form input.
+    /// </summary>
+    /// <param name="placeNumber">Parsed to int, defaults to 0.</param>
+    /// <param name="placeName">-</param>
+    /// <param name="prizeAmount">Parsed to decimal, defaults to 0.</param>
+    /// <param name="prizePercentage">Parsed to double, defaults to 0.</param>
+    public PrizeModel(string placeNumber, string placeName, string prizeAmount, string prizePercentage)
+    {
+        int placeNumberValue = 0;
+        int.TryParse(placeNumber, out placeNumberValue);
+        PlaceNumber = placeNumberValue;
+
+        PlaceName = placeName;
+
+        decimal prizeAmountValue = 0;
+        decimal.TryParse(prizeAmount, out prizeAmountValue);
+        PrizeAmount = prizeAmountValue;
+
+        double prizeAmountPercentage = 0;
+        double.TryParse(prizePercentage, out prizeAmountPercentage);
+        PrizePercentage = PrizePercentage;
+    }
 }
